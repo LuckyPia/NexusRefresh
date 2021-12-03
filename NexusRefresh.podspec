4ba4ac17defcf8f639ae573797aeb4c243b567f6
LuckyPia <664454335@qq.com>
2021-12-03 18:31:12 +0800

    第一次提交

--- NexusRefresh.podspec
+++ NexusRefresh.podspec
@@ -8,8 +8,8 @@
 
 Pod::Spec.new do |s|
   s.name             = 'NexusRefresh'
-  s.version          = '0.1.0'
-  s.summary          = 'A short description of NexusRefresh.'
+  s.version          = '1.0.0'
+  s.summary          = 'iOS多页面数据同步刷新'
 
 # This description is used to generate tags and improve search results.
 #   * Think: What does it do? Why did you write it? What is the focus?
@@ -28,7 +28,8 @@ TODO: Add long description of the pod here.
   s.source           = { :git => 'https://github.com/LuckyPia/NexusRefresh.git', :tag => s.version.to_s }
   # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
 
-  s.ios.deployment_target = '9.0'
+  s.ios.deployment_target = '10.0'
+  s.swift_version = '5.0'
 
   s.source_files = 'NexusRefresh/Classes/**/*'
   
@@ -38,5 +39,6 @@ TODO: Add long description of the pod here.
 
   # s.public_header_files = 'Pod/Classes/**/*.h'
   # s.frameworks = 'UIKit', 'MapKit'
-  # s.dependency 'AFNetworking', '~> 2.3'
+   s.dependency 'RxViewController'
+   s.dependency 'NSObject+Rx'
 end
---
+++ NexusRefresh/Classes/NexusRefreshManager.swift
@@ -0,0 +1,93 @@
+//
+//  NexusManager.swift
+//  Demo
+//
+//  Created by yupao_ios_macmini05 on 2021/12/2.
+//
+
+import NSObject_Rx
+import RxViewController
+import UIKit
+
+// MARK: 纽带刷新管理器
+
+public class NexusRefreshManager {
+    private init() {}
+
+    /// 可用刷新池
+    public static var availableRefreshPool = NSHashTable<AnyObject>(options: .weakMemory)
+
+    /// 等待刷新池
+    static var waitRefreshPool = NSMapTable<AnyObject, NexusRefreshObject>(keyOptions: .weakMemory, valueOptions: .strongMemory)
+
+    /// 添加到可用刷新池
+    public static func add(_ obj: AnyObject) {
+        if let dele = obj as? NexusRefreshDelegate {
+            // 添加到可刷新列表
+            NexusRefreshManager.availableRefreshPool.add(obj)
+
+            if let vc = obj as? UIViewController {
+                // 监听VC出现
+                vc.rx.viewDidAppear.subscribe(onNext: { [weak dele] _ in
+                    guard let `dele` = dele else { return }
+                    // 判断刷新池是否存在
+
+                    if let fromObj = NexusRefreshManager.waitRefreshPool.object(forKey: dele) {
+                        // 刷新
+                        dele.nexusRefresh(data: fromObj.data)
+                        // 移除刷新池
+                        NexusRefreshManager.waitRefreshPool.removeObject(forKey: dele)
+                    }
+                }).disposed(by: vc.rx.disposeBag)
+            }
+        }
+    }
+
+    /// 刷新
+    /// - Parameters:
+    ///   - tags: 标签列表
+    ///   - data: 传递数据
+    ///   - filtObjects: 过滤列表
+    public static func refresh(tags: Set<String>, data: Any? = nil, filtObjects: [AnyObject] = []) {
+        let list = NexusRefreshManager.availableRefreshPool.allObjects
+        for obj in list {
+            if let dele = obj as? NexusRefreshDelegate,
+               dele.nexusTags().intersection(tags).count > 0,
+               !filtObjects.contains(where: { $0.isEqual(obj) })
+            {
+                if let vc = obj as? UIViewController {
+                    // 不可见VC加入等待刷新池，可见VC直接刷新。
+                    if vc.isViewLoaded, vc.view.window != nil {
+                        dele.nexusRefresh(data: data)
+                    } else {
+                        NexusRefreshManager.waitRefreshPool.setObject(NexusRefreshObject(data: data), forKey: obj)
+                    }
+                } else {
+                    // 直接刷新
+                    dele.nexusRefresh(data: data)
+                }
+            }
+        }
+    }
+}
+
+// MARK: 纽带刷新协议
+
+@objc public protocol NexusRefreshDelegate {
+    /// 纽带标签列表
+    func nexusTags() -> Set<String>
+
+    /// 刷新
+    func nexusRefresh(data: Any?)
+}
+
+// MARK: 纽带刷新数据
+
+class NexusRefreshObject {
+    /// 数据
+    var data: Any?
+
+    init(data: Any?) {
+        self.data = data
+    }
+}
