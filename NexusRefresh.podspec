fd4255b9219cafdaccecc959029468c58b780b97
蒲亚东 <664454335@qq.com>
2022-01-06 11:58:09 +0800

    优化代码

--- NexusRefresh.podspec
+++ NexusRefresh.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'NexusRefresh'
-  s.version          = '2.0.2'
+  s.version          = '2.0.3'
   s.summary          = 'iOS多页面数据同步刷新'
 
 # This description is used to generate tags and improve search results.
@@ -22,23 +22,15 @@ TODO: Add long description of the pod here.
                        DESC
 
   s.homepage         = 'https://github.com/LuckyPia/NexusRefresh'
-  # s.screenshots     = 'www.example.com/screenshots_1', 'www.example.com/screenshots_2'
   s.license          = { :type => 'MIT', :file => 'LICENSE' }
   s.author           = { 'LuckyPia' => '664454335@qq.com' }
   s.source           = { :git => 'https://github.com/LuckyPia/NexusRefresh.git', :tag => s.version.to_s }
-  # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
 
   s.ios.deployment_target = '10.0'
   s.swift_version = '5.0'
 
   s.source_files = 'NexusRefresh/Classes/**/*'
   
-  # s.resource_bundles = {
-  #   'NexusRefresh' => ['NexusRefresh/Assets/*.png']
-  # }
-
-  # s.public_header_files = 'Pod/Classes/**/*.h'
-  # s.frameworks = 'UIKit', 'MapKit'
-   s.dependency 'RxViewController'
-   s.dependency 'NSObject+Rx'
+  s.dependency 'RxSwift', '~> 6.2'
+  s.dependency "RxCocoa", "~> 6.2"
 end
--- NexusRefresh/Classes/NexusRefreshManager.swift
+++ NexusRefresh/Classes/NexusRefreshManager.swift
@@ -5,11 +5,11 @@
 //  Created by yupao_ios_macmini05 on 2021/12/2.
 //
 
-import NSObject_Rx
-import RxViewController
+import RxSwift
+import RxCocoa
 import UIKit
 
-// MARK: 纽带刷新管理器
+// MARK: 刷新管理器
 
 public class NexusRefreshManager {
     /// 单例
@@ -18,26 +18,26 @@ public class NexusRefreshManager {
     private init() {}
 
     /// 可用刷新池
-    var availableRefreshPool = NSMapTable<AnyObject, NexusRefreshObserverModel>(keyOptions: .weakMemory, valueOptions: .strongMemory)
+    private var availableRefreshPool = NSMapTable<AnyObject, Observer>(keyOptions: .weakMemory, valueOptions: .strongMemory)
 
     /// 等待刷新池
-    var waitRefreshPool = NSMapTable<AnyObject, NexusRefreshObject>(keyOptions: .weakMemory, valueOptions: .strongMemory)
+    private var waitRefreshPool = NSMapTable<AnyObject, Object>(keyOptions: .weakMemory, valueOptions: .strongMemory)
 
     // MARK: 添加刷新监听
 
     /// 添加刷新监听
     /// - Parameters:
-    ///   - observer: 刷新目标
+    ///   - target: 刷新目标
     ///   - tags: 标签列表
     ///   - refreshBlock: 刷新回调
-    public func add(_ observer: AnyObject, tags: Set<String>, refreshBlock: @escaping (_ data: Any?) -> Void) {
-        let observerModel = NexusRefreshObserverModel(observer: observer, tags: tags, refreshBlock: refreshBlock)
-        NexusRefreshManager.shared.availableRefreshPool.setObject(observerModel, forKey: observer)
+    public func add(_ target: AnyObject, tags: Set<Tag>, refreshBlock: @escaping (_ object: Object) -> Void) {
+        let observerModel = Observer(target: target, tags: tags, refreshBlock: refreshBlock)
+        NexusRefreshManager.shared.availableRefreshPool.setObject(observerModel, forKey: target)
         // 如果是VC进行特殊处理
-        if let vc = observer as? UIViewController {
+        if let vc = target as? UIViewController {
             // VC出现时再刷新
-            vc.rx.viewDidAppear.subscribe(onNext: { [weak observer] _ in
-                guard let observer = observer else { return }
+            vc.rx.nr_viewDidAppear.subscribe(onNext: { [weak target] _ in
+                guard let observer = target else { return }
                 // 判断刷新池是否存在
 
                 if let refreshObject = NexusRefreshManager.shared.waitRefreshPool.object(forKey: observer),
@@ -48,7 +48,7 @@ public class NexusRefreshManager {
                     // 移除等待刷新池
                     NexusRefreshManager.shared.waitRefreshPool.removeObject(forKey: observer)
                 }
-            }).disposed(by: vc.rx.disposeBag)
+            }).disposed(by: vc.rx.nr_disposeBag)
         }
     }
 
@@ -60,21 +60,21 @@ public class NexusRefreshManager {
     ///   - data: 传递数据
     ///   - filtObjects: 过滤列表
     ///   - force: 强制刷新，会直接刷新，不会等待出现（仅对VC有用）
-    public func refresh(tags: Set<String>, data: Any? = nil, filtObjects: [AnyObject] = [], force: Bool = false) {
+    public func refresh(tags: Set<Tag>, data: Any? = nil, filtObjects: [AnyObject] = [], force: Bool = false) {
         // 遍历可刷新池
         NexusRefreshManager.shared.availableRefreshPool.objectEnumerator()?.forEach { object in
             // 判断是否可以刷新
-            if let observerModel = object as? NexusRefreshObserverModel,
+            if let observerModel = object as? Observer,
                tags.intersection(observerModel.tags).count > 0,
                !filtObjects.contains(where: { $0.isEqual(observerModel) })
             {
-                let refreshObject = NexusRefreshObject(tags: tags, data: data)
-                if !force, let vc = observerModel.observer as? UIViewController {
+                let refreshObject = Object(tags: tags, data: data)
+                if !force, let vc = observerModel.target as? UIViewController {
                     // 不可见VC加入等待刷新池；可见VC直接刷新。
                     if vc.isViewLoaded, vc.view.window != nil {
                         observerModel.refreshBlock(refreshObject)
                     } else {
-                        NexusRefreshManager.shared.waitRefreshPool.setObject(refreshObject, forKey: observerModel.observer)
+                        NexusRefreshManager.shared.waitRefreshPool.setObject(refreshObject, forKey: observerModel.target)
                     }
                 } else {
                     // 不是VC直接刷新
@@ -92,38 +92,101 @@ public class NexusRefreshManager {
         NexusRefreshManager.shared.availableRefreshPool.removeObject(forKey: observer)
         NexusRefreshManager.shared.waitRefreshPool.removeObject(forKey: observer)
     }
-}
-
-// MARK: 纽带刷新模型
-
-class NexusRefreshObserverModel {
-    /// 观察者
-    weak var observer: AnyObject?
-
-    /// 标签列表
-    var tags: Set<String> = []
-
-    /// 刷新回调
-    var refreshBlock: (NexusRefreshObject) -> Void
-
-    init(observer: AnyObject, tags: Set<String>, refreshBlock: @escaping (NexusRefreshObject) -> Void) {
-        self.observer = observer
-        self.tags = tags
-        self.refreshBlock = refreshBlock
-    }
-}
-
-// MARK: 纽带刷新数据模型
-
-class NexusRefreshObject {
-    /// 标签列表
-    var tags: Set<String> = []
     
-    /// 数据
-    var data: Any?
+}
 
-    init(tags: Set<String>, data: Any?) {
-        self.tags = tags
-        self.data = data
+// MARK: 刷新管理器扩展
+extension NexusRefreshManager {
+    
+    // MARK: 标签
+    public struct Tag: Hashable, Equatable, RawRepresentable {
+        public var rawValue: String
+
+        public init(_ rawValue: String) {
+            self.rawValue = rawValue
+        }
+
+        public init(rawValue: String) {
+            self.rawValue = rawValue
+        }
+    }
+    
+    // MARK: 纽带观察模型
+
+    class Observer {
+        /// 观察者
+        weak var target: AnyObject?
+
+        /// 标签列表
+        var tags: Set<Tag> = []
+
+        /// 刷新回调
+        var refreshBlock: (Object) -> Void
+
+        init(target: AnyObject, tags: Set<Tag>, refreshBlock: @escaping (Object) -> Void) {
+            self.target = target
+            self.tags = tags
+            self.refreshBlock = refreshBlock
+        }
+    }
+
+    // MARK: 纽带刷新数据模型
+
+    public class Object {
+        /// 标签列表
+        public var tags: Set<Tag> = []
+        
+        /// 数据
+        public var data: Any?
+
+        init(tags: Set<Tag>, data: Any?) {
+            self.tags = tags
+            self.data = data
+        }
     }
 }
+
+// MARK: UIViewController扩展
+public extension Reactive where Base: UIViewController {
+    /// UIViewController将要出现
+    var nr_viewDidAppear: ControlEvent<Bool> {
+        let source = self.methodInvoked(#selector(Base.viewDidAppear)).map { $0.first as? Bool ?? false }
+        return ControlEvent(events: source)
+    }
+}
+
+// MARK: DisposeBag扩展
+fileprivate var nr_disposeBagContext: UInt8 = 0
+
+extension Reactive where Base: AnyObject {
+    func nr_synchronizedBag<T>( _ action: () -> T) -> T {
+        objc_sync_enter(self.base)
+        let result = action()
+        objc_sync_exit(self.base)
+        return result
+    }
+}
+
+extension Reactive where Base: AnyObject {
+
+    /// a unique DisposeBag that is related to the Reactive.Base instance only for Reference type
+    var nr_disposeBag: DisposeBag {
+        get {
+            return nr_synchronizedBag {
+                if let disposeObject = objc_getAssociatedObject(base, &nr_disposeBagContext) as? DisposeBag {
+                    return disposeObject
+                }
+                let disposeObject = DisposeBag()
+                objc_setAssociatedObject(base, &nr_disposeBagContext, disposeObject, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+                return disposeObject
+            }
+        }
+        
+        set {
+            nr_synchronizedBag {
+                objc_setAssociatedObject(base, &nr_disposeBagContext, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            }
+        }
+    }
+}
+
